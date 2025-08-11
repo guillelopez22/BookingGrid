38eb5f59f35c844d5a3d421a364f2a98c7ccb034
Guillermo Lopez <guillelopez_13@hotmail.com>
2025-08-10 20:05:40 -0600

    Import CSS file for styling

--- frontend/src/components/MachineGrid.tsx
+++ frontend/src/components/MachineGrid.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getMachines } from '../services/api';
+import './MachineGrid.css';
 
 const MachineGrid = () => {
   const [machines, setMachines] = useState([]);
